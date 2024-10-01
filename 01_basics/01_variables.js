224c1407b3866fd5507f37dc30c33d1329820bc1
Raziullah Ansari <ransari078.btech2023@mme.nitrr.ac.in>
2024-10-01 10:16:10 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=14453
+let acoountEmail="raj7786@gmail.com"
+var accountPassword="1245"
+accountCity="Delhi"
+
+//  accountId=2 //not allowed
+
+acoountEmail="hafasn@gmail.com"
+accountPassword="212413"
+accountCity="Raipur"
+
+let accountState;
+// console.log(accountId)
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+console.table([accountId,accountPassword,acoountEmail,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use script"//treat all JS code as newer version
+// alert("hello") //we are using node js not browser
+
+console.log(3+3)
+
+
+let name="raj"
+let age=19
+let isLoggedIn=false
+
+//number=>2 to the power 53
+//bigint
+
+//string =>""
+//boolean =>true/false
+//null =>standalone value
+//undefined=>
+//symbol=>unique
+
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof name)
+console.log(typeof age)
+//type of null is object run the below code to check
+console.log(typeof null)
