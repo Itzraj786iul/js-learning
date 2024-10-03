a51e421239ee143f28da1da4c0528452ef9ca0a2
Itsraj786@iul <ransari078.btech2023@mme.nitrr.ac.in>
2024-10-03 15:23:51 +0530

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+//Primitive data types
+
+
+//7 types :String   ,Number,Boolean,null,Undefined,Symbol,BigInt
+const score=100
+const scorevalue=100.3
+const isLoggedIn=false
+const Outsidetemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid);
+
+const bigNumber=2534624312n
+
+
+
+//Refernece (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros=["shaktiman","naagraj","doga"]
+let myObject={
+    name:"raj",
+    age:18,
+
+}
+
+const myFunction=function()
+{
+    console.log("hello world");
+}
+console.log(typeof bigNumber)
